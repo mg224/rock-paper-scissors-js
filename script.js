6f6585948877218aff408090794785543d2f85e1
Misha Gupta <mishagupta@Mishas-MacBook-Air-9.local>
2024-11-03 23:26:28 -0500

    Added ui

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let humanScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.value, getComputerChoice());
+    });
+});
+
+function endGame() {
+    buttons.forEach((button) => {
+        button.disabled = true;
+    });
+}
 
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
@@ -13,27 +26,50 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+    // humanChoice = humanChoice.toLowerCase();
+    let result = '';
+
     if ((humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper')) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+
         humanScore++;
+        result = '<br>You win! ' + humanChoice + ' beats ' + computerChoice + '<br>Player score: ' + humanScore + '<br>Computer score: ' + computerScore;
+        
+
+        if (humanScore === 5) {
+            result += '<br>You won! Reload the page to play again.';
+            endGame();
+        }
+
     }
     else if ((humanChoice == 'rock' && computerChoice == 'paper') || (humanChoice == 'paper' && computerChoice == 'scissors') || (humanChoice == 'scissors' && computerChoice == 'rock')) {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
         computerScore++;
+        result = '<br>You lose! ' + computerChoice + ' beats ' + humanChoice + '<br>Player score: ' + humanScore + '<br>Computer score: ' + computerScore;
+        
+
+        if (computerScore === 5) {
+            result += '<br>You lose! Reload the page to play again.';
+            endGame();
+        }
+        
     }
     else if (humanChoice === computerChoice) {
-        console.log("Tie!");
+
+        result = '<br>Tie! <br>Player score: ' + humanScore + '<br>Computer score: ' + computerScore;
     }
+
+    document.querySelector('#result').innerHTML = result;
+    return;
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-}
+// function playGame() {
+//     for (let i = 0; i < 5; i++) {
+//         playRound(getHumanChoice(), getComputerChoice());
+//     }
+// }
 
-playGame();
+// playGame();
+
+
 
 
 
